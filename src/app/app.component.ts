92b33133ccc9d3626388a0ba953b5a527f5c8a4d
mahantesh <mahantesh@simpragma.com>
2020-12-26 11:13:04 +0530

    Implement i18N

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'facile';
+  constructor(private router: Router, private translate: TranslateService) {
+    this.translate.setDefaultLang('en');
+    this.translate.use('en');
+  }
 }
+
+
